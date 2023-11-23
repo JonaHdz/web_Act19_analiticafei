9f11644fb6815f147a4556f972dfbab5dd4cd7d7
Jona <zS19023589@estudiantes.uv.mx>
2023-11-22 19:10:02 -0600

    a

---
+++ AnaliticaFEI/AnaliticaFEI/Models/TarjetaViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnaliticaFEI.Models
+{
+    public class TarjetaViewModel
+    {
+        [Required(ErrorMessage = "EL campo {0} es obligatorio")]
+        [Display(Name = "Tarjeta de credito")]
+        public string Card_number { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Nombre como aparece en la tarjeta")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Expiracion")]
+        public string Expiry { get; set; }
+        [Required(ErrorMessage = "EL campo {0} es ibligatorio")]
+        [StringLength(3, ErrorMessage = "EL campo {0} debe ser una cadena con un maximo de {1} caracteres")]
+        [Display(Name ="CVV")]
+        public string Cvv { get; set; }
+    
+    }
+}
---
+++ AnaliticaFEI/AnaliticaFEI/wwwroot/js/home.js
@@ -0,0 +1,16 @@
+﻿//paso 13
+const form = document.querySelector('form');
+const inputCreditNumber = document.getElementById('Card_number');
+const inputExpity = document.getElementById('Expiry');
+
+inputCreditNumber.addEventListener('keyup', function (e) {
+    $cc.validate(e)
+});
+
+inputExpity.addEventListener('keyup', function (e) {
+    $cc.expiry.call(this,e)
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
